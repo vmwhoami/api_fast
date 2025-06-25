d6a93a3736f67d6da7e8154691ba0cfe6e6ed9ac
vmwhoami <vmwhoami@gmail.com>
2025-06-25 21:27:15 +0300

    working variant

--- app/main.py
+++ app/main.py
@@ -1,34 +1,69 @@
-from fastapi import FastAPI
-from . import schemas, models
-from .database import engine
-from typing import Optional
+from typing import Annotated
 
-# import uvicorn
-# models.Base.metadata.ceate_all(engine)
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret_name: str
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
-@app.get("/blog")
-async def index(limit=10, pusblished: bool = True, sort: Optional[str]=None):
-    if pusblished:
-        return {"data": f" published {pusblished}"}
-    else:
-         return {"data": f" limit {limit}"}
 
-@app.get("/blog/unpublished")
-def unpublished():
-    return { "data": {"unpublished": "all unpublished"}}
-    
-
-@app.get("/blog/{id}")
-def read_item(id: int):
-    return {"data": {"id": id}}
- 
-@app.post("/blog")
-def create(request: schemas.Blog):
-    return {"data": f"The blog title is {request.title} {request.body}"}
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 
-# DEBUGING PURPOSE ONLY
-# if __name__=="__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=3000)
+@app.post("/heroes/")
+def create_hero(hero: Hero, session: SessionDep) -> Hero:
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
+    return hero
+
+
+@app.get("/heroes/")
+def read_heroes(session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=100)] = 100,) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get("/heroes/{hero_id}")
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}

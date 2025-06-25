cec5c1690f88ec014698d3be559e8b45c50b63b3
vmwhoami <vmwhoami@gmail.com>
2025-06-25 09:17:38 +0300

    change requirments file

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+
+### Install requirments.txt file
+
+```
+ pip install -r requirements.txt
+```
+
+## Run the app comand
+
+```
+ uvicorn app.main:app --reload
+```
+
--- app/main.py
+++ app/main.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/blog")
+async def index(limit=10, pusblished: bool = True, sort: Optional[str]=None):
+    if pusblished:
+        return {"data": f" published {pusblished}"}
+    else:
+         return {"data": f" limit {limit}"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/blog/unpublished")
+def unpublished():
+    return { "data": {"unpublished": "all unpublished"}}
+    
+
+@app.get("/blog/{id}")
+def read_item(id: int):
+    return {"data": {"id": id}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blog")
+ 
+def create_blog(request: Blog):
+    return {"data": f"The blog title is {request.title}"}
+
+
+# DEBUGING PURPOSE ONLY
+# if __name__=="__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=3000)

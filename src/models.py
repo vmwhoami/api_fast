ea080061e6a216887fa04e99f6c2877fa0683382
vmwhoami <vmwhoami@gmail.com>
2025-06-26 16:26:00 +0300

    add some relationthips

--- src/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from sqlmodel import Field, Session, SQLModel,create_engine, select
-from .database import Base
-class Training(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
-    duration: Integer
--- src/schemas.py
+++
@@ -1,9 +0,0 @@
- 
-from fastapi import Depends, FastAPI, HTTPException, Query
-from pydantic import BaseModel
-from typing import Optional
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-class Blog(BaseModel):
-    title: str
-    body: str
-    published: Optional[bool]

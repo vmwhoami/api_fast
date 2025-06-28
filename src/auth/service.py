d3be05562b2f537192820e05ee71706d73fd4d40
vmwhoami <vmwhoami@gmail.com>
2025-06-28 13:44:04 +0300

    add env variables

--- src/auth/service.py
+++ src/auth/service.py
@@ -7,16 +7,17 @@ import jwt
 from jwt import PyJWTError
 from sqlalchemy.orm import Session
 from src.entities.user import User
+import os
 from . import model
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from ..exceptions import AuthenticationError
 import logging
 
 # You would want to store this in an environment variable or a secret manager
-SECRET_KEY = '197b2c37c391bed954e80344fe73b806947a65e36206e05a1a23c2fa12702fe3'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM =  os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
